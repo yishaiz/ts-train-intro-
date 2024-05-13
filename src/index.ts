7d8168e0ab3ccb12c11df385e90e94af2cc3156a
yishai <yishai5555@gmail.com>
2024-05-13 17:04:10 +0300

    04-generics

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,30 @@
 // A FIFO collection
-class Queue {
+class Queue<T> {
   data = [];
-  push(item) {
+  push(item: T) {
     this.data.push(item);
   }
 
-  pop() {
+  pop(): T {
     return this.data.shift();
   }
 }
 
-class NumberQueue extends Queue {
-  push(item: number) {
-    super.push(item);
-  }
+// class NumberQueue extends Queue {
+//   push(item: number) {
+//     super.push(item);
+//   }
 
-  pop(): number {
-    return super.pop();
-  }
-}
+//   pop(): number {
+//     return super.pop();
+//   }
+// }
 
-const queue = new NumberQueue();
+// const queue = new Queue();
+
+const queue = new Queue<number>();
 queue.push(123);
-queue.push(456);
+queue.push('Hello World');
 
 console.log(queue.pop().toPrecision(1));
 console.log(queue.pop().toPrecision(1));
